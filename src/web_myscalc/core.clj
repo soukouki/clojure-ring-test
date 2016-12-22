6f7fa07442f83549b5acdafd71b1620219c3c4b1
soukouki <soukouki0@yahoo.co.jp>
2016-12-23 01:14:27 +0900

    viewをファイル分割した

--- src/web_myscalc/core.clj
+++ src/web_myscalc/core.clj
@@ -1,31 +1,15 @@
 (ns web_myscalc.core
 	(:require
-		[clojure.string :as str]
-		[clojure.java.shell :as sh]
-		[clojure.java.io :as io]
-		[ring.adapter.jetty :as server]))
+		[ring.adapter.jetty :as server]
+		[web_myscalc.view :as v]))
 
 (defonce server (atom nil))
-
-(defn search-view []
-	(slurp (io/file (io/resource "search.html"))))
-		
-(defn myscalc [f]
-	(:out (sh/sh "myscalc.cmd" :in f)))
-
-(defn calc-view [uri]
-	(str (str/replace (myscalc (subs uri 1)) #"\r\n" "<br>") "<br><a href='/'>return</a>"))
-
-(defn select-view [uri]
-	(case uri
-		"/" (search-view)
-		(calc-view uri)))
 
 (defn handler [req] (println (:uri req))
 	{
 	:status 200,
 	:headers {"Content-Type" "text/html"},
-	:body (select-view (:uri req))})
+	:body (v/select-view (:uri req))})
 
 (defn start-server []
 	(when-not @server
